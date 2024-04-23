76d835f7db2a6919f79b74c7630d7f9b3c29a36e
tithe <tith3@proton.me>
2024-04-23 18:47:17 -0400

    Post preview component

---
+++ src/lib/components/PostPreview.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import type { PostMetadata } from '$lib/posts';
+    export let post: PostMetadata;
+</script>
+
+<a class ="block card card-hover p-4" href="/posts/{post.slug}">
+    <h2 class="text-2xl font-bold">{post.title}</h2>
+    <!-- Post content truncated to 2 lines ending in ... if truncated -->
+    <p class="text-lg">{post.content.split('\n').slice(0, 2).join(' ')}</p>
+</a>
