af8378ed189a2a084d8ea7d6856b29cceedf34b6
tithe <tith3@proton.me>
2024-04-23 18:50:57 -0400

    Moved parent div to layout and added postpreview for all posts. It does not work because getPosts does not work.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,9 @@
 <script lang='ts'>
 	import { Avatar } from "@skeletonlabs/skeleton";
+	import {getPosts} from '$lib/posts';
+	import PostPreview from '$lib/components/PostPreview.svelte';
 </script>
-<div class='max-w-prose mx-auto flex-col items-center bg-surface-50-900-token size-full gap-10 space-y-20 py-20'>
+
 	
 	<Avatar src="https://pbs.twimg.com/profile_images/1741149508120322048/tVNiWJ8f_400x400.jpg"
 	 
@@ -9,4 +11,6 @@
 	 class="mx-auto "
 	 rounded="rounded-none"/>
 	<p class="text-lg">Hi I'm tithe. By day I do software in traditional finance (boring). I'm mostly interested in web3, but this is intedended to act as a repository for all of my terrible ideas, web3 or otherwise. Make yourself at home.</p>
-	</div>
+	{#each getPosts() as post}
+		<PostPreview {post}/>
+	{/each}
