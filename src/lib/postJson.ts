19f26a1428aa92daae66d1d18e4fdd61996b59c8
tithe <tith3@proton.me>
2024-04-25 18:05:31 -0400

    Output of posts build script. contains posts in json form.

---
+++ src/lib/postJson.ts
@@ -0,0 +1,110 @@
+export const posts = [
+  {
+    "title": "Advanced Formatting",
+    "author": "Ivy Kim",
+    "publishDate": "2024-04-12",
+    "editDate": "2024-04-13",
+    "tags": [
+      "test",
+      "hello"
+    ],
+    "slug": "advanced-formatting",
+    "content": "<p><strong>Markdown</strong> is <em>really</em> <em>versatile</em>!</p>\n<p><del>Strikethrough</del> is also possible.</p>\n"
+  },
+  {
+    "title": "Code Blocks in Markdown",
+    "author": "Henry Moore",
+    "publishDate": "2024-04-13",
+    "editDate": "2024-04-14",
+    "tags": [
+      "yes",
+      "no"
+    ],
+    "slug": "code-blocks-in-markdown",
+    "content": "<pre><code class=\"language-javascript\">console.log(&#39;Hello, world!&#39;);\n</code></pre>\n"
+  },
+  {
+    "title": "Creating Links",
+    "author": "Eve Martinez",
+    "publishDate": "2024-04-16",
+    "editDate": "2024-04-17",
+    "tags": [
+      "no",
+      "goodbye"
+    ],
+    "slug": "creating-links",
+    "content": "<h5>Links in Markdown</h5>\n<p><a href=\"https://www.google.com\">Google</a></p>\n"
+  },
+  {
+    "title": "Learning SvelteKit",
+    "author": "Bob Smith",
+    "publishDate": "2024-04-19",
+    "editDate": "2024-04-20",
+    "tags": [
+      "no",
+      "test"
+    ],
+    "slug": "learning-sveltekit",
+    "content": "<h2>SvelteKit Tips</h2>\n<p>Here&#39;s how you can use <code>code</code> in Markdown.</p>\n"
+  },
+  {
+    "title": "Lists in Markdown",
+    "author": "Dave Brown",
+    "publishDate": "2024-04-17",
+    "editDate": "2024-04-18",
+    "tags": [
+      "test",
+      "hello"
+    ],
+    "slug": "lists-in-markdown",
+    "content": "<h4>Lists</h4>\n<ul>\n<li>Item 1</li>\n<li>Item 2<ul>\n<li>Subitem 2.1</li>\n<li>Subitem 2.2</li>\n</ul>\n</li>\n</ul>\n"
+  },
+  {
+    "title": "Markdown and Images",
+    "author": "Carol White",
+    "publishDate": "2024-04-18",
+    "editDate": "2024-04-19",
+    "tags": [
+      "yes",
+      "goodbye"
+    ],
+    "slug": "markdown-and-images",
+    "content": "<h3>Inserting Images</h3>\n<p><img src=\"/path/to/image.jpg\" alt=\"Alt Text\"></p>\n"
+  },
+  {
+    "title": "Markdown Tables",
+    "author": "Frank Green",
+    "publishDate": "2024-04-15",
+    "editDate": "2024-04-16",
+    "tags": [
+      "yes",
+      "test"
+    ],
+    "slug": "markdown-tables",
+    "content": "<h6>Markdown Table Example</h6>\n<table>\n<thead>\n<tr>\n<th>Syntax</th>\n<th>Description</th>\n</tr>\n</thead>\n<tbody><tr>\n<td>Header</td>\n<td>Title</td>\n</tr>\n<tr>\n<td>Paragraph</td>\n<td>Text</td>\n</tr>\n</tbody></table>\n"
+  },
+  {
+    "title": "My First Post",
+    "author": "Alice Johnson",
+    "publishDate": "2024-04-20",
+    "editDate": "2024-04-21",
+    "tags": [
+      "yes",
+      "hello"
+    ],
+    "slug": "my-first-post",
+    "content": "<h1>Welcome to My Blog</h1>\n<p>This is a test of <strong>bold text</strong> and <em>italic text</em>.</p>\n"
+  },
+  {
+    "title": "Quoting in Markdown",
+    "author": "Grace Lee",
+    "publishDate": "2024-04-14",
+    "editDate": "2024-04-15",
+    "tags": [
+      "hello",
+      "goodbye"
+    ],
+    "slug": "quoting-in-markdown",
+    "content": "<blockquote>\n<p>Blockquotes are very handy in email to emulate reply text.</p>\n</blockquote>\n"
+  }
+];
