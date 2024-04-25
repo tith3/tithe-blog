a0bcca08891823a21625001fa9c302bee66463f2
tithe <tith3@proton.me>
2024-04-25 18:07:48 -0400

    Clamped preview to 2 lines, add publish data, and some other small changes.

--- src/lib/components/PostPreview.svelte
+++ src/lib/components/PostPreview.svelte
@@ -3,8 +3,11 @@
     export let post: PostMetadata;
 </script>
 
-<a class ="block card card-hover p-4" href="/posts/{post.slug}">
-    <h2 class="text-2xl font-bold">{post.title}</h2>
-    <!-- Post content truncated to 2 lines ending in ... if truncated -->
-    <p class="text-lg">{post.content.split('\n').slice(0, 2).join(' ')}</p>
+<a class ="block card card-hover p-4 flex space-x-2" href="/posts/{post.slug}">
+    <div class="flex flex-col grow">
+        <h2 class="text-lg font-bold">{post.title}</h2>
+        <!-- Post content truncated to 2 lines ending in ... if truncated -->
+        <p class="text-sm line-clamp-2">{@html post.content.split('\n').slice(0, 2).join(' ')}</p>
+    </div>
+    <p> {post.publishDate} </p>
 </a>
