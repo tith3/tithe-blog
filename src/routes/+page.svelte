960fd1edb90e2363ffbc92f7e52ce412744e3f62
tithe <tith3@proton.me>
2024-04-25 18:06:41 -0400

    Changes to bio and using PostPreviewContainer instead of manually displaying post previews.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,8 @@
 <script lang='ts'>
 	import { Avatar } from "@skeletonlabs/skeleton";
-	import {getPosts} from '$lib/posts';
-	import PostPreview from '$lib/components/PostPreview.svelte';
+	import PostPreviewContainer from '$lib/components/PostPreviewContainer.svelte';
+	import { type PostMetadata, getPosts } from '$lib/posts';
+	
 </script>
 
 	
@@ -10,7 +11,5 @@
 	 width="w-1/2"
 	 class="mx-auto "
 	 rounded="rounded-none"/>
-	<p class="text-lg">Hi I'm tithe. By day I do software in traditional finance (boring). I'm mostly interested in web3, but this is intedended to act as a repository for all of my terrible ideas, web3 or otherwise. Make yourself at home.</p>
-	{#each getPosts() as post}
-		<PostPreview {post}/>
-	{/each}
+	<p class="text-lg">Hi I'm tithe. By day I do software in traditional finance (boring). I'm mostly interested in web3, but this is intended to act as a repository for all of my terrible ideas, web3 or otherwise. Feel free to check out my github or twitter as well.</p>
+	<PostPreviewContainer paginatePosts={false}/>
