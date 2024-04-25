43aa5c3e707d3c9ee31370a35001a55e4d9471e5
tithe <tith3@proton.me>
2024-04-25 18:05:00 -0400

    Component to hold post previews with optional pagination. Tag system not yet implemented.

---
+++ src/lib/components/PostPreviewContainer.svelte
@@ -0,0 +1,52 @@
+
+<script lang='ts'>
+    import PostPreview from "$lib/components/PostPreview.svelte";
+    import { getPostsByTags,  type PostMetadata, getTags } from "$lib/posts";
+    import { Paginator } from "@skeletonlabs/skeleton";
+    //display posts only with the following tags
+    export let tags: string[] = getTags();
+
+    //number of posts to display (per page if paginated)
+    export let numPosts: number = 5;
+
+    //whether to paginate posts
+    export let paginatePosts: boolean = false;
+
+    function sortPostsByPublishDate(posts: PostMetadata[]): PostMetadata[] {
+        return posts.sort((a, b) => {
+            return new Date(b.publishDate).getTime() - new Date(a.publishDate).getTime();
+        });
+    }
+
+    let posts = sortPostsByPublishDate(getPostsByTags(tags));
+ 
+    let paginationSettings = {
+        page: 0,
+        limit: numPosts,
+        size: posts.length,
+        amounts: [5, 10, 15, 20, 30, 50]
+        };
+    
+
+    $: paginatedSource = posts.slice(
+        paginationSettings.page * paginationSettings.limit,
+        paginationSettings.page * paginationSettings.limit + paginationSettings.limit
+    );
+</script>
+
+<div class="space-y-4 flex flex-col px-1" >
+        {#each paginatedSource as post, i}
+            <PostPreview {post}/>
+        {/each}
+        {#if paginatePosts}
+            <Paginator 
+                bind:settings={paginationSettings}
+                showFirstLastButtons="{false}"
+                showPreviousNextButtons="{true}"
+                justify="justify-between"
+             />
+        {:else}
+            <a class="btn btn-sm variant-ghost w-fit self-center" href="/posts" >All Posts -&#62;</a>
+        {/if}
+    
+</div>
