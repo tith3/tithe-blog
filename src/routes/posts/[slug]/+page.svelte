a6d505715269f58e3f8af45d29fea5675e20ae2c
tithe <tith3@proton.me>
2024-06-09 18:04:34 -0400

    Fixed issue where text was the wrong color in dark mode using applyClass action.

--- src/routes/posts/[slug]/+page.svelte
+++ src/routes/posts/[slug]/+page.svelte
@@ -1,18 +1,22 @@
 <script lang="ts">
     import { page } from '$app/stores';
+	import { applyClass } from '$lib/actions/applyClass';
     import { getPostFromSlug } from '$lib/posts';
     let slug = $page.params.slug;
 
     function getPost(){
         if (slug){
+
             return getPostFromSlug(slug)?.content;
         }
         else{
-            return '<h1>UH OH</h1>'
+            return '<h1 class="h1 not-prose">UH OH</h1>'
         }
     }
 </script> 
 
 <div>
-    {@html getPost()}
+    <article class="prose" use:applyClass={'dark:text-on-surface-token'}>
+        {@html getPost()}
+    </article>
 </div>
